4612c2184e8cfa228f6e8d5aa6bfdb54c270a58b
Pete Maynard <pgm987@gmail.com>
2013-02-01 20:07:03 +0000

    Added support for OpenID

--- www/index.php
+++ www/index.php
@@ -1,5 +1,4 @@
-<?php require 'openid.php'; ?>
-
+<?php require_once 'openid.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -49,38 +48,54 @@
     <div class="container">
 
       <h1>Learn Something New! <blink>Now!</blink></h1>
-<?php 
-try {
-    # Change 'localhost' to your domain name.
-    $openid = new LightOpenID('port22.co.uk');
-    if(!$openid->mode) {
-        if(isset($_GET['login'])) {
-            $openid->identity = 'https://www.google.com/accounts/o8/id';
-            $openid->required = array('contact/email');
-            $openid->optional = array('namePerson', 'namePerson/friendly');
-            header('Location: ' . $openid->authUrl());
-        }
-?>
-      <form action="?login" method="post">
-        <button>Login with Google</button> <a href="http://openidexplained.com" target="_BLANK">What is Open ID?</a>
+
+      <?php
+      try {
+          # Change 'localhost' to your domain name.
+          $openid = new LightOpenID('port22.co.uk');
+          if(!$openid->mode) {
+              if(isset($_POST['openid_identifier'])) {
+                  $openid->identity = $_POST['openid_identifier'];
+                  header('Location: ' . $openid->authUrl());
+              }
+      ?>
+
+      <form action="" method="post" id="openid_form">
+        <input type="hidden" name="action" value="verify" />
+        <fieldset>
+          <legend>Sign-in or Create New Account</legend>
+          <div id="openid_choice">
+            <p>Please click your account provider:</p>
+            <div id="openid_btns"></div>
+          </div>
+          <div id="openid_input_area">
+            <input id="openid_identifier" name="openid_identifier" type="text" value="http://" />
+            <input id="openid_submit" type="submit" value="Sign-In"/>
+          </div>
+          <noscript>
+            <p>OpenID is service that allows you to log-on to many different websites using a single indentity.
+            Find out <a href="http://openid.net/what/">more about OpenID</a> and <a href="http://openid.net/get/">how to get an OpenID enabled account</a>.</p>
+          </noscript>
+        </fieldset>
       </form>
 
       <?php
-    } elseif($openid->mode == 'cancel') {
-        echo 'User has canceled authentication!';
-    } else {
-        echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
-        print_r($openid->getAttributes());
-    }
-} catch(ErrorException $e) {
-    echo $e->getMessage();
-} ?>
+          } elseif($openid->mode == 'cancel') {
+              echo 'User has canceled authentication!';
+          } else {
+              echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
+          }
+      } catch(ErrorException $e) {
+          echo $e->getMessage();
+      }
+      ?>
       
-
     </div> <!-- /container -->
+    <script src="assets/js/jquery-1.9.0.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
 
+    <!-- OpenID Selector -->
     <script src="assets/js/openid-jquery.js"></script>
     <script src="assets/js/openid-en.js"></script>
   </body>
